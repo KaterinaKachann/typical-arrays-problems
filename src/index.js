34a670e2c2f9330692353de8f21b3da3203f2cf5
Katerina Kachan <eka.kachan@gmail.com>
2020-10-03 23:07:41 +0300

    feat:add new solution task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (!array || array.length < 1) {
+    return 0;
+  }
+
+  let min = array[0];
+  for (let i = 1; i < array.length; i++) {
+      current = array[i];
+      if (current < min) {
+          min = current;
+      }
+  }
+  return min;
 }
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.max = function max(array) {
+  if (!array || array.length < 1) {
+    return 0;
+  }
+  let max = array[0];
+  for (let i = 1; i < array.length; i++) {
+      current = array[i];
+      if (current > max) {
+          max = current;
+      }
+  }
+  return max;
+};
+exports.avg = function avg(array) {
+  if (!array || array.length < 1) {
+    return 0;
+  }
 
-exports.avg = function avg (array) {
-  return 0;
-}
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+      sum += array[i];
+  }
+  let avg =sum / array.length;
+  return avg;
+};
